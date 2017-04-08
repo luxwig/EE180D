0c143ecaf7fd85011fb3578b484a4494a41949f0
Ludwig Zhao <luxwig@gmail.com>
2017-04-08 16:59:54 -0700

    finish interface for realtime

---
+++ cf.h
@@ -0,0 +1,34 @@
+#ifndef CF_H
+#define CF_H
+
+#include "global.h"
+#include "util.h"
+#include <pthread.h>
+#include <signal.h>
+
+#define _DATA_ACQ_SIZE  8
+#define _MAX_BUF_SIZE   65536
+
+sig_atomic_t volatile r_flag = 1;
+
+int bufpos  = 0;
+int bufsize = 0;
+
+pthread_mutex_t cv_lock = PTHREAD_MUTEX_INITIALIZER,
+                slock = PTHREAD_MUTEX_INITIALIZER;
+
+pthread_cond_t cv = PTHREAD_COND_INITIALIZER;
+
+
+void do_signal(int sig)
+{
+    if (sig == SIGINT)
+        r_flag = 0;
+    printf("BREAK\n");
+}
+
+
+void* data_acq(void*);
+void* data_pro(void*);
+
+#endif
--- global.h
+++ global.h
@@ -1,14 +1,12 @@
 #ifndef GLOBAL_H
 #define GLOBAL_H
 
-#define _DATA_ACQ_SIZE 8
-#define _MAX_BUF_SIZE 65536
-#define _WAKE_UP_SPEED 100
 #define _BUFFER 65536
 #define _SBUFFER 256
 #define _TRAIN_DATA_SIZE 10
 #define _TEST_DATA_SIZE 10
-const char [_TRAIN_DATA_SIZE][_SBUFFER]TRAINING_DATASET ={"data/WALK/ludwig/1.csv",  
+
+const char TRAINING_DATASET [_TRAIN_DATA_SIZE][_SBUFFER]={"data/WALK/ludwig/1.csv",  
                          "data/WALK/ludwig/2.csv",
                          "data/WALK/ludwig/3.csv",
                          "data/WALK/ludwig/4.csv",
@@ -17,9 +15,9 @@ const char [_TRAIN_DATA_SIZE][_SBUFFER]TRAINING_DATASET ={"data/WALK/ludwig/1.cs
                          "data/ASCEND/3.csv",
                          "data/DESCEND/1.csv",
                          "data/DESCEND/2.csv",
-                         "data/DESCEND/3.csv"}
+                         "data/DESCEND/3.csv"};
 
-const char [_TEST_DATA_SIZE][_SBUFFER]TEST_DATASET={
+const char TEST_DATASET[_TEST_DATA_SIZE][_SBUFFER]={
                          "data/WALK/ludwig/1_t.csv",  
                          "data/WALK/ludwig/2_t.csv",
                          "data/WALK/ludwig/3_t.csv",
